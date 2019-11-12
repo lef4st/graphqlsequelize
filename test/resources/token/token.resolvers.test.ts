fc31913112a253784bd5c26ff7b1ac46e2e370e2
LEANDRO ROLA BELAS PEREIRA <leandrorbpereira@gmail.com>
2019-11-12 11:06:19 -0300

    Reupload do projeto graphqlsequelize
    Reupload do projeto graphqlsequelize

---
+++ test/resources/token/token.resolvers.test.ts
@@ -0,0 +1,119 @@
+import * as jwt from 'jsonwebtoken';
+
+import { app, db, chai, handleError, expect } from './../../test-utils';
+import { UserInstance } from '../../../src/models/UserModel';
+import { JWT_SECRET } from '../../../src/utils/utils';
+import { PostInstance } from '../../../src/models/PostModel';
+import { CommentInstance } from '../../../src/models/CommentModel';
+
+describe('Token', () => {
+
+    beforeEach(() => {
+        return db.Comment.destroy({ where: {} })
+            .then((rows: number) => db.Post.destroy({ where: {} }))
+            .then((rows: number) => db.User.destroy({ where: {} }))
+            .then((rows: number) => db.User.create(
+                {
+                    name: 'Peter Quill',
+                    email: 'peter@guardians.com',
+                    password: '1234'
+                }
+            )).catch(handleError);
+    });
+
+    describe('Mutations', () => {
+
+        describe('application/json', () => {
+
+            describe('createToken', () => {
+
+                it('should return a new valid token', () => {
+
+                    let body = {
+                        query: `
+                            mutation createNewToken($email: String!, $password: String!) {
+                                createToken(email: $email, password: $password) {
+                                    token
+                                }
+                            }
+                        `,
+                        variables: {
+                            email: 'peter@guardians.com',
+                            password: '1234'
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            expect(res.body.data).to.have.key('createToken');
+                            expect(res.body.data.createToken).to.have.key('token');
+                            expect(res.body.data.createToken.token).to.be.string;
+                            expect(res.body.errors).to.be.undefined;
+                        }).catch(handleError);
+                });
+
+                it('should return an error if the password is incorrect', () => {
+
+                    let body = {
+                        query: `
+                            mutation createNewToken($email: String!, $password: String!) {
+                                createToken(email: $email, password: $password) {
+                                    token
+                                }
+                            }
+                        `,
+                        variables: {
+                            email: 'peter@guardians.com',
+                            password: 'WRONG_PASSWORD'
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            expect(res.body).to.have.keys(['data', 'errors']);
+                            expect(res.body.data).to.have.key('createToken');
+                            expect(res.body.data.createToken).to.be.null;
+                            expect(res.body.errors).to.be.an('array').with.length(1);
+                            expect(res.body.errors[0].message).to.equal('Unauthorized, wrong e-mail or password!');
+                        }).catch(handleError);
+                });
+
+                it('should return an error when the email not exists', () => {
+
+                    let body = {
+                        query: `
+                            mutation createNewToken($email: String!, $password: String!) {
+                                createToken(email: $email, password: $password) {
+                                    token
+                                }
+                            }
+                        `,
+                        variables: {
+                            email: 'ronan@guardians.com',
+                            password: '1234'
+                        }
+                    };
+
+                    return chai.request(app)
+                        .post('/graphql')
+                        .set('content-type', 'application/json')
+                        .send(JSON.stringify(body))
+                        .then(res => {
+                            expect(res.body).to.have.keys(['data', 'errors']);
+                            expect(res.body.data).to.have.key('createToken');
+                            expect(res.body.data.createToken).to.be.null;
+                            expect(res.body.errors).to.be.an('array').with.length(1);
+                            expect(res.body.errors[0].message).to.equal('Unauthorized, wrong e-mail or password!');
+                        }).catch(handleError);
+                });
+            })
+        })
+    });
+
+});
